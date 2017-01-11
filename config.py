da4b665c1d683f12b7ac99e95e42272f6b4d1052
c26 <c26root@gmail.com>
2017-01-11 23:25:22 +0800

    修改配置项 注释

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# API主机列表
 hosts = [
 	'localhost:8775',
 	'127.0.0.1:8775'
 ]
+
+# 超时时间
 timeout = 5
+
 user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"
 
 headers = {
@@ -16,4 +20,5 @@ headers = {
     'X-Real-IP': '8.8.8.8'
 }
 
+# 需要污染的头部
 pollution_headers = ('Referer', 'User-Agent', 'X-Forwarded-For', 'Client-IP', 'X-Real-IP', )
